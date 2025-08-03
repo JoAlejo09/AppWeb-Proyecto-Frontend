0f063d2ee6bba8bc89511a3f885bded2a4f24615
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-08-03 17:43:06 -0500

    ROLLBACK - COMMIT

--- src/components/RutaProtegida.jsx
+++ src/components/RutaProtegida.jsx
@@ -1,15 +1,14 @@
 import { Navigate } from "react-router-dom";
-import storeAuth from "../context/storeAuth";
+import storeAuth from "../context/storeAuth.js";
 
 const RutaProtegida = ({ children, rol }) => {
-  const { token, rol: rolUsuario } = storeAuth(); // ✅ esta es la línea correcta
+  const { token, rol: rolUsuario } = storeAuth();
 
-  if (!token || !rolUsuario) {
-    return <Navigate to="/login" replace />;
-  }
-
-  if (rol && rolUsuario !== rol) {
-    return <Navigate to="/login" replace />;
+  if (!token || rolUsuario !== rol) {
+    return null;
+    }
+  if(rolUsuario!==rol){
+        return <Navigate to="/login" replace />;
   }
 
   return children;
