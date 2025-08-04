52f3adf4879f4682972b26f1429da700e939d1bc
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-08-04 00:11:22 -0500

    UPDATE SOCKET

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,10 @@
 // src/socket.js
 import { io } from "socket.io-client";
+import storeAuth from "../store/storeAuth";
 
-const token = localStorage.getItem("token"); // o desde Zustand si prefieres
+const token = storeAuth.getState().token; // Obtiene el token del estado de Zustand
+
+// Crea la instancia de Socket.io con el token de autenticación
 
 export const socket = io("https://mentalapp-backend-rqqe.onrender.com", {
   auth: {
