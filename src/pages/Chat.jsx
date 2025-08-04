1b45b9445cb1e9954f953f9f4a043c3c1d12a671
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-08-03 23:01:20 -0500

    UPDATE CHAT

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -34,7 +34,7 @@ const Chat = () => {
     useEffect(() => {
           const token = localStorage.getItem("token"); // o desde stwworeAuth
 
-        const newSocket = io("https://app-web-proyecto-frontend.vercel.app",{
+        const newSocket = io("https://mentalapp-backend-rqqe.onrender.com",{
             auth:{
                 token,
             },
