05fd336ac0e1932294ef713f3d05c0179e141f24
Jose Pila <josealejandro_pilavizuete@hotmail.com>
2025-08-04 00:00:53 -0500

    UPDATE STOREAUTH

--- src/store/storeAuth.js
+++ src/store/storeAuth.js
@@ -1,11 +1,20 @@
+// src/store/storeAuth.js
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const storeAuth = create((set) => ({
-  token: null,
-  user: null,
-  rol: null,
-  setAuth: ({ token, user, rol }) => set({ token, user, rol }),
-  logout: () => set({ token: null, user: null, rol: null }),
-}));
+const storeAuth = create(
+  persist(
+    (set) => ({
+      token: null,
+      user: null,
+      rol: null,
+      setAuth: ({ token, user, rol }) => set({ token, user, rol }),
+      logout: () => set({ token: null, user: null, rol: null }),
+    }),
+    {
+      name: 'auth-storage', // nombre de la clave en localStorage
+    }
+  )
+);
 
 export default storeAuth;
